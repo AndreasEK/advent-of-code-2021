2a20ef5cacd1bbce6b2b5bb071fb5c5b6758b0a9
Andreas Ebbert-Karroum <andreas@karroum.de>
2021-12-14 11:20:56 +0100

    day 2 refactoring

--- day2/day2.py
+++ day2/day2.py
@@ -2,17 +2,19 @@ command = input()
 horizontal = 0
 depth = 0
 aim = 0
+
 while command != "STOP":
-    distance = int(command.split()[1])
-    if command.startswith("forward"):
+    direction, distance = command.split()
+    distance = int(distance)
+    if direction == "forward":
         horizontal += distance
         depth += aim * distance
-    if command.startswith("down"):
+    if direction == "down":
         aim += distance
-    if command.startswith("up"):
+    if direction == "up":
         aim -= distance
     command = input()
 
-print(f"horizontal = {horizontal}")
-print(f"depth =  {depth}")
-print(f"Multiplied: {horizontal * depth}")
+    print(f"horizontal = {horizontal}")
+    print(f"depth =  {depth}")
+    print(f"Multiplied: {horizontal * depth}")
